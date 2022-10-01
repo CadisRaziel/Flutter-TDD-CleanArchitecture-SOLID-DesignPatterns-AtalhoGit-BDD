9640712cd6d6e3b45991499ae27e84cdb2e18511
Vitor <vitorcsharp@hotmail.com>
2022-10-01 17:14:12 -0300

    move production code to their own target

---
+++ lib/data/http/http_client.dart
@@ -0,0 +1,7 @@
+abstract class HttpClient {
+  Future<void>? request({
+    String url,
+    String method,
+    Map? body,
+  });
+}
--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -7,8 +7,14 @@ abstract class Authentication {
 class AuthenticationParams {
   final String email;
   final String secret;
+
   AuthenticationParams({
     required this.email,
     required this.secret,
   });
+
+  Map toJson() => {
+        'email': email,
+        'password': secret,
+      };
 }
